ffa9a4a44402f527e8b323b65673caddc1f8beed
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-04-02 21:29:23 +0300

    draft_01

--- Lesson 06/script06_homework.js
+++ Lesson 06/script06_homework.js
@@ -1,1 +1,40 @@
 'use strict';
+ 
+const gameNum = function(n){
+    let guess = n;
+    let value = prompt("Введите число", '');
+        
+    function base(){
+        if (parseFloat(value) > guess){
+            alert('Загаданное число меньше, попробуйте еще раз!', '');
+        } else {
+            if (parseFloat(value) < guess){
+                alert('Загаданное число больше, попробуйте еще раз!', '');
+            } else {
+                if(parseFloat(value) === guess){
+                return alert('Вы угадали!');
+                } else {
+                    return base(n);
+                }
+            }
+        }
+    }
+
+    let firstSign = value.slice(0, 1);
+    if(firstSign > 0 || firstSign < 0){ //если число
+    base();
+        } else {
+             alert('Введите число!');
+        }        
+};
+
+gameNum(5);
+console.dir(gameNum());
+
+    
+
+
+
+
+// — если пользователь ввел не число, то выводит сообщение "Введи число!" и предлагает ввести новый вариант;
+// — если пользователь нажимает "Отмена", то игра заканчивается.
--- Lesson 06/script06_lecture.js
+++ Lesson 06/script06_lecture.js
@@ -1,1 +1,41 @@
 'use strict';
+// Вложенность
+let y = 10;
+
+function one(){
+    function two(){
+        console.log(y);
+    }
+    two();
+}
+one();
+
+//Видимость
+// 1. Сначала ищет значение внутри функций, являющихся lexical enviroment;
+// 2. Потом lexical enviroment;
+// 3. Затем scope (lexical enviroment материнской функции);
+// 4. global scope (lexical enviroment всего скрипта);
+
+//ЗАМЫКАНИЕ
+
+let a = 50;
+ function one(){
+     let x = 10;
+     function two(y){
+        function three(){
+            let y = 5;
+            return x * y;
+        }
+        return x + y + three;
+     }
+     console.dir(two);
+     return two(15);
+ }
+
+ function funcMath(a) {
+     return function(b) {
+         console.log(a * b);
+     };
+ }
+ const mathPow = funcMath(6);
+ mathPow(5);
