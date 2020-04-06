ea4ba7d30241cc7a437125ec7529a233ee2932f6
asheSibir <56887042+asheSibir@users.noreply.github.com>
2020-04-06 09:50:03 +0300

    Обязательная домашка - script07_homework.js

--- Lesson 07/script07_homework.js
+++ Lesson 07/script07_homework.js
@@ -1,25 +1,27 @@
 'use strict';
-//УНИВЕРСАЛЬНАЯ ФОРМУЛА
 const isNum = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 let money,
-start = function(){
-    do{
-        money = prompt('Ваш месячный доход?','987654');
-    }
-    while(isNaN(money) || money === '' || money === null);
-};   
+    start = function(){
+        do{
+            money = prompt('Ваш месячный доход?','987654');
+        }
+        while(isNaN(money) || money === '' || money === null);
+    };   
 start();
     
-let expenses1 = prompt('Введите обязательную статью расходов?','Жилье'),
-    amount1 = prompt('Во сколько это обойдется?',''),
+let 
+    
     expenses2 = prompt('Введите обязательную статью расходов?','Питание'), 
     amount2 = prompt('Во сколько это обойдется?','');
  
 
 let appData = {
+    budget: money,
+    budgetDay: 0,
+    budgetMonth: 0,
     income: {}, 
     addIncome: [],
     expenses: {},
@@ -31,96 +33,54 @@ let appData = {
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
         appData.addExpenses = addExpenses.toLowerCase().split('. ');
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
-        appData.getExpensesMonth = function(){
-            if (isNum(amount1) === true && isNum(amount2) === true){
-                alert (amount1/1 + amount2/1);
-            } else {
-                alert('Введите число!'); //Проследите чтобы тип данных значения свойств были числом!
-            }
-        };
-    }
-};
-appData.asking(); //7.6) Сразу после объекта выполните вызов appData.asking();
+        for (let i = 0; i < 2; i++){
+            let expenses = prompt('Введите обязательную статью расходов?','Жилье');
+            let amount;
+            do{
+                amount = prompt('Во сколько это обойдется?','');
+            } 
+            while (isNaN(amount) || amount === '' || amount === null);
 
-//7.7) Перенести цикл из метода getExpensesMonth в метод asking, 
-//и переписать цикл таким образом чтобы результат записывался в объект  appData.expenses
-appData.expenses = {};
-appData.expenses['ответ на первый вопрос: '] = [+amount1];
-appData.expenses['ответ на второй вопрос: '] = [+amount2];
+            appData.expenses[expenses] = amount;
+        }
+    },
 
-// const getExpensesMonth = function (a,b){
-//     return((a / 1) + (b / 1));
-// };
-// alert('Расходы за месяц - ' + getExpensesMonth(amouexpensesnt1, amount2));
-
-appData.budget = money;
-appData.budgetDay = 0; 
-appData.budgetMonth = 0;
-appData.expensesMonth = 0;
-//7.8) Переписать метод getExpensesMonth: с помощью цикла считаем сумму всех обязательных расходов 
-//и сохраняем результат в свойство expensesMonth нашего объекта
-//для того, чтобы посчитать сумму используйте цикл for in
-
-appData.expensesMonth = function(){
-    if (isNum(appData.expenses['ответ на первый вопрос: ']) === true && isNum(appData.expenses['ответ на второй вопрос: ']) === true){
-        for(let arg in appData.expenses){
-            return(appData.expenses['ответ на первый вопрос: ']/1 + appData.expenses['ответ на второй вопрос: ']/1);
+    getExpensesMonth: function(){
+        for (let key in appData.expenses) {
+            appData.expensesMonth += +appData.expenses[key];
+        }
+    },
+    getBudget: function () {
+        appData.budgetMonth = appData.getBudget - appData.expensesMonth;
+        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+    },
+    getTargetMonth: function () {
+        if (appData.budgetDay > 800) {
+            return ('Высокий уровень дохода');
+        } else if (appData.budgetDay > 300) {
+            return ('Средний уровень дохода');
+        } else if (appData.budgetDay > 0) {
+            return ('Низкий уровень дохода')
+        } else {
+            return('Что-то пошло не так!');
         }
     }
+    
 };
+appData.asking(); 
+appData.getExpensesMonth();
+appData.getBudget();
 
+console.log('Расходы за месяц: ' + appData.expensesMonth);
 
-//7.9) getAccumulatedMonth переименовать в getBudget. 
-//Этот метод будет высчитывать значения свойств budgetMonth и budgetDay, 
-//чтобы вычислить значения используем только свойства объекта (никаких внешних переменных)
-
-let getAccumulatedMonth = function(){
-    return((money / 1) - (appData.expenses['ответ на первый вопрос: ']/1) - (appData.expenses['ответ на второй вопрос: ']/1));
-  };
-
-
-let getBudget = {
-    budgetMonth: [getAccumulatedMonth()]
-};
-getBudget.budgetDay = [getBudget.budgetMonth] / 30;
-
-
-
-appData.getTargetMonth = function(){
-        return (appData.mission / appData.accumulatedMonth);
-};
-
-// appData.getStatusIncome = function(); ТАКОЙ ФУНКЦИИ НЕ БЫЛО
-
-//12) В консоль вывести: 
-//— Расходы за месяц
-//— За какой период будет достигнута цель (в месяцах)
-//— Уровень дохода
-
-console.log('Расходы за месяц: ' + appData.expensesMonth());
-console.log('Цель будет достигнута за '+ Math.ceil(appData.mission / appData.expensesMonth()) + ' месяцев');
-console.log('Уровень дохода - ' + (getBudget.budgetDay > 5000? 'высокий' : 'низкий'));
-
-//13) Используя цикл for in для объекта (appData), 
-//вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести все свойства и значения)
-
-// let keys = Object.keys(appData)
-let counter = 0;
-for (let key in appData) {
-  counter++;
-}
-console.log(counter);
-
-for (let key in appData) {
-    let value = appData[key];
-    console.log(key + ": " + value); 
+if (appData.getTargetMonth() > 0) {
+    console.log('Цель будет достигнута за ' + Math.ceil(app.getTargetMonth()) + 'месяцев');
+} else {
+    console.log('Цель не будет достигнута');
 }
 
+console.log(appData.getStatusIncome());
 
-
-
-
-
-
-
-
+for (let key in appData){
+    console.log('Наша программа включает в себя данные: ' + key + ' - ' + appData[key]);
+}
